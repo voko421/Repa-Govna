e27a70712f0bb86836b3c4aab19e080040192065
PetrosanW <cerberuswaress14@gmail.com>
2025-01-27 03:33:10 +0300

    They took a shit

--- Marsey/Stealthsey/Abjure.cs
+++ Marsey/Stealthsey/Abjure.cs
@@ -1,18 +1,25 @@
 using Marsey.Config;
+using Marsey.Misc;
 
 namespace Marsey.Stealthsey;
 
 public static class Abjure
 {
-    private static Version? engineVer { get; set; } 
-    
+    private static Version? engineVer { get; set; }
+
     /// <summary>
     /// Checks against version with detection methods
     /// </summary>
     /// <returns>True if version is equal or over with detection and hidesey is disabled</returns>
     public static bool CheckMalbox(string engineversion, HideLevel MarseyHide)
     {
-        engineVer = new Version(engineversion);
+        engineversion = engineversion.Split('-')[0];
+
+        if (!Version.TryParse(engineversion, out var v))
+        {
+            MarseyLogger.Log(MarseyLogger.LogType.ERRO, $"Incorrect version format: {engineversion}");
+            return false;
+        }
 
         return engineVer >= MarseyVars.Detection && MarseyHide == HideLevel.Disabled;
     }
